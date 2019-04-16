477c1c9c3952b0ddbfc2061af2075b7bd7adf571
Nicolas Finkelstein <nicolasfinkelstein2020@u.northwestern.edu>
2019-04-16 11:54:27 -0500

    readded next song button

--- src/Components/PlayList/NowPlaying.js
+++ src/Components/PlayList/NowPlaying.js
@@ -1,51 +1,27 @@
 import React, { Component } from "react";
-import "../../Styles/NowPlaying.css"
+import "../../Styles/NowPlaying.css";
 
 // Might have to change to class based when we add admin buttons?
-
-class NowPlaying extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currSong: {
-        artistName: this.props.currSong.artistName,
-        songName: this.props.currSong.songName,
-        albumName: this.props.currSong.albumName,
-      },
-    };
-  }
-
-  render(){
-    return(
-      <div className="np-container">
-        <div className="np-imagecontainer"></div>
-        <div className="np-info">
-          <div className="np-info-songname">{this.props.currSong.songName}</div>
-          <div className="np-info-artist">{this.props.currSong.artistName}</div>
-        </div>
+const NowPlaying = ({ currSong, nextSong }) => {
+  return (
+    <div className="np-container">
+      <div className="np-imagecontainer">
+        {" "}
+        {currSong.songAlbum !== "N/A" ? (
+          <img src={currSong.songAlbum} width="100%" aref="Song Album" />
+        ) : (
+          ""
+        )}
       </div>
-    )
-  }
-}
-
+      <div className="np-info">
+        <div className="np-info-songname">{currSong.songName}</div>
+        <div className="np-info-artist">{currSong.songArtist}</div>
+        <button style={{ marginBottom: "10px" }} onClick={nextSong}>
+          Next Song
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default NowPlaying;
-
-
-// import React from "react";
-
-// // Might have to change to class based when we add admin buttons?
-// const Playlist = ({ currSong }) => {
-//   return (
-//     <div>
-//       <h2 style={{ textDecoration: "underline" }}>Now Playing</h2>
-//       <h4>Song: {currSong.songName}</h4>
-//       <h4>Artist: {currSong.artistName}</h4>
-//       <h4>Album: {currSong.albumName}</h4>
-//       <p> ____________________ </p>
-//     </div>
-//   );
-// };
-
-// export default Playlist;
