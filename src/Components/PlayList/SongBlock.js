99c0c6d6e8758e06369bd2928ffc27ef44a53237
FrancisYL <yiansu66@gmail.com>
2019-04-20 20:15:27 -0500

    likedBy now can be updated on Firebase

--- src/Components/PlayList/SongBlock.js
+++ src/Components/PlayList/SongBlock.js
@@ -7,31 +7,49 @@ const db = app.database();
 class SongBlock extends Component {
   // Like button is clicked -> will either remove current uid from likedBy or add current uid to likedBy
   // this will trigger the on_changed event in App.js
+  // handleLike(songId) {
+  //   db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId)
+  //     .child("likedBy")
+  //     .orderByValue()
+  //     .equalTo(this.props.uid)
+  //     .once("value", snapshot => {
+  //       console.log(snapshot.exists());
+  //       // Snapshot will exist if user has already liked the song before
+  //       if (!snapshot.exists()) {
+  //         // Add user to likedBy (like song)
+  //         db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId)
+  //           .child("likedBy")
+  //           .push(this.props.uid);
+  //       } else {
+  //         // Remove user from likedBy (dislike song)
+  //         var keyToRemove = Object.keys(snapshot.val())[0];
+  //         db.ref(
+  //           "playlists/" +
+  //             this.props.playlistKey +
+  //             "/songs/" +
+  //             songId +
+  //             "/likedBy/"
+  //         )
+  //           .child(keyToRemove)
+  //           .remove();
+  //       }
+  //     });
+  // }
+
   handleLike(songId) {
     db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId)
       .child("likedBy")
-      .orderByValue()
+      .orderByChild("uid")
       .equalTo(this.props.uid)
       .once("value", snapshot => {
-        console.log(snapshot.exists());
-        // Snapshot will exist if user has already liked the song before
         if (!snapshot.exists()) {
-          // Add user to likedBy (like song)
-          db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId)
-            .child("likedBy")
-            .push(this.props.uid);
+          db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId + "/likedBy")
+            .push()
+            .set({
+              uid: this.props.uid
+            });
         } else {
-          // Remove user from likedBy (dislike song)
-          var keyToRemove = Object.keys(snapshot.val())[0];
-          db.ref(
-            "playlists/" +
-              this.props.playlistKey +
-              "/songs/" +
-              songId +
-              "/likedBy/"
-          )
-            .child(keyToRemove)
-            .remove();
+          alert('You already voted');
         }
       });
   }
