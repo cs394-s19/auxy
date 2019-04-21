60776f13cfc726d6f09c4b2ef139321d78ac0756
Jonathan Dai <jonathandai1226@gmail.com>
2019-04-20 20:58:28 -0500

    updated landing page UI

--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import "../../Styles/Homepage.css";
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+
 // import { Link,withRouter, Route } from "react-router-dom";
 
 
@@ -32,19 +37,48 @@ class Homepage extends Component {
 
   render() {
     return (
-      <div>
+      <div className='background'>
+      <div className='top-circle'></div>
+      <div className='bottom-circle'></div>
+        <div className='welcome-container'>
+        <div className='meet-text'>MEET</div>
+          <div className='auxy-text'>AUXY</div>
+          <div className='sub-text'>a playlist for your occasion, by your occasion</div>
+          <StyledButton onClick={this.handleHostClick}>Host</StyledButton>
+          <StyledButton onClick={this.handleUserClick}>join</StyledButton>
+          </div>
+         
           <div>
-            <input onChange={e => {this.handleHostInput(e);}}/>
-            <button onClick={this.handleHostClick}>Host</button>
+            {/* <input onChange={e => {this.handleHostInput(e);}}/> */}
+            {/* <button onClick={this.handleHostClick}>Host</button> */}
           </div>
           <div>
-            <input onChange={e => {this.handleUserInput(e);}}/>
-            <button onClick={this.handleUserClick}>Join</button>
+            {/* <input onChange={e => {this.handleUserInput(e);}}/> */}
+            {/* <button onClick={this.handleUserClick}>Join</button> */}
           </div>
       </div>
     );
   }
 }
+
+const StyledButton = withStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 48,
+    border: 0,
+    color: 'white',
+    height: '48px',
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    width: '120px',
+    marginTop: '15px',  
+    marginRight: '18px',  
+    zIndex:'2',
+  },
+  label: {
+    textTransform: 'lowercase',
+  },
+})(Button);
 
 // export default withRouter(Homepage);
 export default Homepage;
