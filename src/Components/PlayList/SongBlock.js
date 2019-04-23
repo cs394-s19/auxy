a5689a474bb050a25d2f30fb5701e746ec12f8a6
FrancisYL <yiansu66@gmail.com>
2019-04-23 12:00:13 -0500

    enable Nico's implementation

--- src/Components/PlayList/SongBlock.js
+++ src/Components/PlayList/SongBlock.js
@@ -7,52 +7,52 @@ const db = app.database();
 class SongBlock extends Component {
   // Like button is clicked -> will either remove current uid from likedBy or add current uid to likedBy
   // this will trigger the on_changed event in App.js
-  // handleLike(songId) {
-  //   db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId)
-  //     .child("likedBy")
-  //     .orderByValue()
-  //     .equalTo(this.props.uid)
-  //     .once("value", snapshot => {
-  //       console.log(snapshot.exists());
-  //       // Snapshot will exist if user has already liked the song before
-  //       if (!snapshot.exists()) {
-  //         // Add user to likedBy (like song)
-  //         db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId)
-  //           .child("likedBy")
-  //           .push(this.props.uid);
-  //       } else {
-  //         // Remove user from likedBy (dislike song)
-  //         var keyToRemove = Object.keys(snapshot.val())[0];
-  //         db.ref(
-  //           "playlists/" +
-  //             this.props.playlistKey +
-  //             "/songs/" +
-  //             songId +
-  //             "/likedBy/"
-  //         )
-  //           .child(keyToRemove)
-  //           .remove();
-  //       }
-  //     });
-  // }
-
   handleLike(songId) {
     db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId)
       .child("likedBy")
-      .orderByChild("uid")
+      .orderByValue()
       .equalTo(this.props.uid)
       .once("value", snapshot => {
+        console.log(snapshot.exists());
+        // Snapshot will exist if user has already liked the song before
         if (!snapshot.exists()) {
-          db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId + "/likedBy")
-            .push()
-            .set({
-              uid: this.props.uid
-            });
+          // Add user to likedBy (like song)
+          db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId)
+            .child("likedBy")
+            .push(this.props.uid);
         } else {
-          alert('You already voted');
+          // Remove user from likedBy (dislike song)
+          var keyToRemove = Object.keys(snapshot.val())[0];
+          db.ref(
+            "playlists/" +
+              this.props.playlistKey +
+              "/songs/" +
+              songId +
+              "/likedBy/"
+          )
+            .child(keyToRemove)
+            .remove();
         }
       });
   }
+
+  // handleLike(songId) {
+  //   db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId)
+  //     .child("likedBy")
+  //     .orderByChild("uid")
+  //     .equalTo(this.props.uid)
+  //     .once("value", snapshot => {
+  //       if (!snapshot.exists()) {
+  //         db.ref("playlists/" + this.props.playlistKey + "/songs/" + songId + "/likedBy")
+  //           .push()
+  //           .set({
+  //             uid: this.props.uid
+  //           });
+  //       } else {
+  //         alert('You already voted');
+  //       }
+  //     });
+  // }
 
   handleDelete(songId) {
     db.ref("playlists/" + this.props.playlistKey)
